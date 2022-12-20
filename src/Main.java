c2d57e0f945c1e280de74dbc3825d115d4cae915
username <abc.123@gmail.com>
2022-12-20 21:38:04 +0000

    Featrue sun has been added but it has some error

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
        XX.moon=1;
        XX.sun=1;
        XX.Planet=2;
+
+       FeatureSun Xeron = new FeatureSun();
+       Xeron.Heat=("33434");
+       Xeron.sixe=(54444);
+       Xeron.Temperature=(54432222);
     }
 }
